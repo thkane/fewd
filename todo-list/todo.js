21db91da10978142d5b99d2265d79461b3596670
thkane <thkane@gmail.com>
2016-10-04 23:16:12 -0700

    Bonus! Working!

--- todo-list/todo.js
+++ todo-list/todo.js
@@ -12,21 +12,28 @@ form.addEventListener("submit", addToDo);
 function addToDo(event) {
   event.preventDefault();
 
-  //add new todo
-  var new_li = document.createElement("li");
-  toDoHolder.appendChild(new_li);
-  var new_label = document.createElement("label");
-  new_li.appendChild(new_label);
-  var new_input = document.createElement("input");
-  new_label.appendChild(new_input);
-  new_input.setAttribute("type","checkbox");
-  var newToDo = document.createElement("span");
-  new_label.appendChild(newToDo);
-  newToDo.textContent = " " + todoEntry.value;
+  var toDoArray = todoEntry.value.split(",");
 
-  //update todo count
-  var count = parseInt(toDoNumber.textContent);
-  toDoNumber.textContent = count + 1;
+  for (var i = 0; i < toDoArray.length; i++) {
+    var new_li = document.createElement("li");
+    toDoHolder.appendChild(new_li);
+    var new_label = document.createElement("label");
+    new_li.appendChild(new_label);
+    var new_input = document.createElement("input");
+    new_label.appendChild(new_input);
+    new_input.setAttribute("type","checkbox");
+    var newToDo = document.createElement("span");
+    new_label.appendChild(newToDo);
+    newToDo.textContent = " " + toDoArray[i];
+
+    //update todo count
+    var count = parseInt(toDoNumber.textContent);
+    toDoNumber.textContent = count + 1;
+
+  }
+
+
+  //add new todo
 
   //hide default text
   var nothing = document.querySelector(".nothing");
