6cf191ad15d15b5e6f11b542a38c5a6713e62897
thkane <thkane@gmail.com>
2016-10-20 17:25:03 -0700

    Geo working

--- strava/routes_old.js
+++ strava/routes_old.js
@@ -1,48 +1,34 @@
 
 var generateSegment = document.querySelector("#go");
-var findMe = document.querySelector("#findme");
 
 generateSegment.addEventListener("click", findSegment);
-findMe.addEventListener("click", geoFind);
-
-
-
-function geoFind(event) {
-
-  navigator.geolocation.getCurrentPosition(storeCoord);
-
-  function storeCoord(position) {
-    var latitude_start  = position.coords.latitude;
-    var longitude_start = position.coords.longitude;
-    console.log(latitude_start);
-    document.querySelector("#lat").value = latitude_start;
-    document.querySelector("#long").value = longitude_start;
-  }
-}
-
-
 
 function findSegment(event) {
   event.preventDefault();
 
-    var latitude_start  = parseInt(document.querySelector("#lat").value);
-    var longitude_start = parseInt(document.querySelector("#long").value);
+  function success(position) {
+
+    var latitude_start  = position.coords.latitude;
+    var longitude_start = position.coords.longitude;
 
     var area = parseInt(document.querySelector("#area").value);
     var max_cat = parseInt(document.querySelector("#climb").value);
 
     var latitude_end = latitude_start + (area * 0.01455445222);
-    var longitude_end = longitude_start + (area * 0.01455445222);
+    var longitude_end = longitude_end + (area * 0.01455445222);
+
+    var output = document.querySelector("#out");
+    output.innerHTML = '<div class="data">Latitude is <span id="lat1">' + latitude_start + '</span>°<br>Longitude is <span id="long1">' + longitude_start + '</span>°</div>';
 
     var strava_url = "https://crossorigin.me/https://www.strava.com/api/v3/segments/explore?access_token=c95ed5932a9cddcb6595abaaccf6ddb099ea49ff&bounds=" + latitude_start + "," + longitude_start + "," + latitude_end + "," + longitude_end + "&max_cat=" + max_cat;
     $.get(strava_url, listSegments);
+
     var maps_url = "https://www.google.com/maps/embed/v1/place?q=";
     var apiKey = "AIzaSyDmqqsWbwD5-3iahKJ39g_yr9et3Ml70mc";
 
     function listSegments(results) {
       var segments = results.segments;
       var parent = document.querySelector(".stravaresults");
-      parent.style.display = "block";
 
       for (var i = 0; i < segments.length; i++) {
         var newSegment = document.createElement("div");
@@ -54,3 +40,7 @@ function findSegment(event) {
     }
 
   }
+
+navigator.geolocation.getCurrentPosition(success);
+
+}
