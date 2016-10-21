000fdaa893c96005305879a71e6e0ad11b98fd4a
Tim Kane <thkane@gmail.com>
2016-10-21 14:58:41 -0700

    AJAX loader and graphic opacity

--- strava/routes.js
+++ strava/routes.js
@@ -5,7 +5,12 @@ var findMe = document.querySelector("#findme");
 generateSegment.addEventListener("click", findSegment);
 findMe.addEventListener("click", geoFind);
 
+$body = $("body");
 
+$(document).on({
+  ajaxStart: function() { $body.addClass("loading");    },
+  ajaxStop: function() { $body.removeClass("loading"); }
+});
 
 function geoFind(event) {
 
@@ -14,7 +19,6 @@ function geoFind(event) {
   function storeCoord(position) {
     var latitude_start  = position.coords.latitude;
     var longitude_start = position.coords.longitude;
-    console.log(latitude_start);
     document.querySelector("#lat").value = latitude_start;
     document.querySelector("#long").value = longitude_start;
   }
@@ -34,27 +38,42 @@ function findSegment(event) {
 
     var area = parseInt(document.querySelector("#area").value);
     var max_cat = parseInt(document.querySelector("#climb").value);
+    // var max_cat = 5;
 
     var latitude_end = latitude_start + (area * 0.01455445222);
     var longitude_end = longitude_start + (area * 0.01455445222);
 
     var strava_url = "https://crossorigin.me/https://www.strava.com/api/v3/segments/explore?access_token=c95ed5932a9cddcb6595abaaccf6ddb099ea49ff&bounds=" + latitude_start + "," + longitude_start + "," + latitude_end + "," + longitude_end + "&max_cat=" + max_cat;
     $.get(strava_url, listSegments);
-    console.log(strava_url);
     var maps_url = "https://www.google.com/maps/embed/v1/place?q=";
     var apiKey = "AIzaSyDmqqsWbwD5-3iahKJ39g_yr9et3Ml70mc";
 
     function listSegments(results) {
       var segments = results.segments;
-      var parent = document.querySelector(".stravaresults");
-      // parent.style.display = "block";
+      var parent = document.querySelector("body");
+      var stravaresults = document.createElement("div");
+      stravaresults.classList.add("stravaresults");
+      parent.appendChild(stravaresults);
+      stravaresults.innerHTML = '<div class="resulttotal">We found ' + segments.length + ' segments for you to check out!</div>'
+
 
       for (var i = 0; i < segments.length; i++) {
         var newSegment = document.createElement("div");
         newSegment.classList.add("segment");
+        var parent = document.querySelector(".stravaresults");
         parent.appendChild(newSegment);
-        newSegment.innerHTML = '<div class="segmentdata"><p>Segment name: <a href="https://www.strava.com/segments/'  + segments[i].id + '" target="_blank">' + segments[i].name + '</a></p><p>Climb rating: ' + segments[i].climb_category + '</p><p>Distance: ' + (segments[i].distance/1000).toFixed(1) + ' km</p></div><div class="map"><iframe width="300" height="225" frameborder="0" style="border:0" src=' + maps_url + segments[i].start_latlng[0] + "%20" +  segments[i].start_latlng[1] + "&key=" + apiKey + ' allowfullscreen></iframe></div>';
+        newSegment.innerHTML = '<div class="segmentdata"><p>Segment name: <a href="https://www.strava.com/segments/'
+        + segments[i].id + '" target="_blank">' + segments[i].name + '</a></p><p>Climb rating: '
+        + segments[i].climb_category + '</p><p>Distance: ' + (segments[i].distance/1000).toFixed(1)
+        + ' km</p></div><div class="map"><iframe width="300" height="225" frameborder="0" style="border:0" src='
+        + maps_url + segments[i].start_latlng[0] + "%20" +  segments[i].start_latlng[1] + "&key=" + apiKey
+        + ' allowfullscreen></iframe></div>';
       }
+//       $("#go").click(function() {
+//           $('html, body').animate({
+//         scrollTop: $(".stravaresults").offset().top
+//     }, 2000);
+// });
 
     }
 
